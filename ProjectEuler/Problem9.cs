fdc33d644e0c6ca6186028ecf0718c5d853d50bf
Scott Millar <scottmillar95@Gmail.com>
2015-11-16 02:17:06 +0000

    Graphicsssss

---
+++ ProjectEuler/Problem9.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int a=3; a <= 1000; a++)
+            {
+                for(int b=4;b <= 1000; b++)
+                {
+                    for(int c=5;c <= 1000; c++)
+                    {
+                        if((a*a) + (b*b) == c*c)
+                        {
+                            if (a + b + c == 1000)
+                            {
+                                int product = a * b * c;
+                                Console.WriteLine(a + " " + b + " " + c + " " + product);
+                            }
+                        } 
+                    }
+                }
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ ProjectEuler/Program10.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* The Sum of the primes below 10 is 2 + 3 + 5 + 7 = 17
+   Find the sum of all the primes below two million */
+
+namespace Problem_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 0;
+
+            for (int i = 0; i <= 2000000; i++)
+            {
+                if(isPrime(i))
+                {
+                    total += i;
+                    Console.WriteLine(total + " " + i);
+                }
+            }
+
+            Console.WriteLine(total);
+            Console.Read();
+        }
+
+        public static bool isPrime(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return false;
+            }
+            for (int i = 3; i * i <= n; i += 2)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
